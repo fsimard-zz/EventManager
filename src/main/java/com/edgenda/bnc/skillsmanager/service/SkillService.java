67c5f86960198d45843a337ce39ea4d72b4a49e3
Francis Simard <simf003@C02YJ0KMJG5H.local>
2019-05-07 10:24:45 -0400

    fix build

--- src/main/java/com/edgenda/bnc/skillsmanager/service/SkillService.java
+++ src/main/java/com/edgenda/bnc/skillsmanager/service/SkillService.java
@@ -1,14 +1,18 @@
 package com.edgenda.bnc.skillsmanager.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.edgenda.bnc.skillsmanager.model.Employee;
+import com.edgenda.bnc.skillsmanager.model.Skill;
 import com.edgenda.bnc.skillsmanager.repository.EmployeeRepository;
 import com.edgenda.bnc.skillsmanager.repository.SkillRepository;
 import com.edgenda.bnc.skillsmanager.service.exception.UnknownSkillException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-
-import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 @Transactional
