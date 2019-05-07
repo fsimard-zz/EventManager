d61b69dc3ea222ab680b6325a4f5d79483c317d7
Gagnon Hane <gagnon.hane@bnc.ca>
2019-05-07 09:58:34 -0400

    Guest Service Init

--- src/main/java/com/edgenda/bnc/skillsmanager/service/GuestService.java
+++ src/main/java/com/edgenda/bnc/skillsmanager/service/GuestService.java
@@ -1,8 +1,23 @@
 package com.edgenda.bnc.skillsmanager.service;
 
+import com.edgenda.bnc.skillsmanager.repository.EmployeeRepository;
+import com.edgenda.bnc.skillsmanager.repository.EventRepository;
+import com.edgenda.bnc.skillsmanager.repository.GuestRepository;
+import com.edgenda.bnc.skillsmanager.repository.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GuestService {
+
+    private final GuestRepository guestRepository;
+    private final EventRepository eventRepository;
+
+
+    @Autowired
+    public GuestService(GuestRepository guestRepository, EventRepository eventRepository) {
+        this.guestRepository = guestRepository;
+        this.eventRepository = eventRepository;
+    }
 
 }
