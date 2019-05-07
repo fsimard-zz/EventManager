0cae89d787977e542295518ca61b446083abab46
willy2019051 <william_lykoy@hotmail.com>
2019-05-07 10:00:15 -0400

    Adding EventController CRUD and addGuesToEvent

---
+++ src/main/java/com/edgenda/bnc/skillsmanager/service/EventService.java
@@ -0,0 +1,30 @@
+package com.edgenda.bnc.skillsmanager.service;
+
+import com.edgenda.bnc.skillsmanager.model.Event;
+import com.edgenda.bnc.skillsmanager.model.Skill;
+
+import java.util.List;
+
+public class EventService {
+    public static List<Event> getEvents() {
+        return null;
+    }
+
+    public static List<Event> getEvent(Long id) {
+        return null;
+    }
+
+    public Event createEvent(Event event) {
+        return null;
+    }
+
+    public List<Skill> eventGuest(Long id) {
+        return null;
+    }
+
+    public void updateEvent(Event event) {
+    }
+
+    public void deleteEvent(Long id) {
+    }
+}
